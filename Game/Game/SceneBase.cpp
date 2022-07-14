a4c365334b02df2e1a1969c24309303ab8d1756f
calumice <99gastgamer81@gmail.com>
2022-07-14 22:09:52 +0900

    SceneBaseの.cppと.hの追加
    安藤のコミット

---
+++ Game/Game/SceneBase.cpp
@@ -0,0 +1,5 @@
+#include "SceneBase.h"
+
+SceneBase::~SceneBase()
+{
+}
---
+++ Game/Game/SceneBase.h
@@ -0,0 +1,39 @@
+#pragma once
+
+/// <summary>
+/// それぞれシーンのタグ.
+/// </summary>
+enum SCENE_TAG
+{
+	NONE_TAG = 0,
+	TITLE_TAG,
+	PLAY_TAG,
+	RESULT_TAG,
+};
+
+/// <summary>
+/// SceneBaseクラス.
+/// </summary>
+class SceneBase
+{
+	/// <summary>
+	/// コンストラクタ.
+	/// </summary>
+	SceneBase();
+
+	/// <summary>
+	/// デストラクタ.
+	/// </summary>
+	virtual ~SceneBase();
+
+	/// <summary>
+	/// 更新関数、仮想関数.
+	/// </summary>
+	/// <returns>enumのSCENE_TAGを返す.</returns>
+	virtual SCENE_TAG Update() = 0;
+
+	/// <summary>
+	/// 描画関数、仮想関数.
+	/// </summary>
+	virtual void Draw() = 0;
+};
