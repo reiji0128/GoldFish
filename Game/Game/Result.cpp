339b76ddb56e89c022e9e6fecc2f93c5febbdf3d
calumice <99gastgamer81@gmail.com>
2022-08-22 22:03:55 +0900

    リザルトシーンの追加
    安藤のコミット

--- Game/Game/Result.cpp
+++ Game/Game/Result.cpp
@@ -1,1 +1,39 @@
 #include "Result.h"
+
+/// <summary>
+/// コンストラクタ.
+/// </summary>
+Result::Result()
+{
+}
+
+/// <summary>
+/// デストラクタ.
+/// </summary>
+Result::~Result()
+{
+}
+
+/// <summary>
+/// 更新関数.
+/// </summary>
+/// <param name="deltaTime">float型のデルタタイムの引数.</param>
+/// <returns>SCENE_TAG型のenumクラスを返す.</returns>
+SCENE_TAG Result::Update(float deltaTime)
+{
+	// タイトルシーンへの移行処理.@@@
+
+	// ゲーム終了処理.@@@
+
+	// それ以外はこのシーンを返す.
+	return SCENE_TAG::NONE_TAG;
+}
+
+/// <summary>
+/// 描画関数.
+/// </summary>
+void Result::Draw()
+{
+	// 背景の描画処理.
+	mBg->Draw();
+}
