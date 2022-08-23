cea032a01553fd254460e715d2482291fdfdea42
calumice <99gastgamer81@gmail.com>
2022-08-23 15:49:57 +0900

    gameManagerの追加
    安藤のコミット

---
+++ Game/Game/GameManager.cpp
@@ -0,0 +1,34 @@
+#include "GameManager.h"
+
+/// <summary>
+/// コンストラクタ.
+/// </summary>
+GameManager::GameManager()
+{
+	// こちらに変数のコンストラクタ処理を入力してください.
+}
+
+/// <summary>
+/// デストラクタ.
+/// </summary>
+GameManager::~GameManager()
+{
+	// こちらに変数のデストラクタ処理を入力してください.
+}
+
+/// <summary>
+/// 更新関数.
+/// </summary>
+/// <param name="deltaTime"></param>
+void GameManager::Update(float deltaTime)
+{
+	// こちらに変数の更新処理を入力してください.
+}
+
+/// <summary>
+/// 描画関数.
+/// </summary>
+void GameManager::Draw()
+{
+	// こちらに変数の描画処理を入力してください.
+}
