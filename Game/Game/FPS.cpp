9ec6f8dc76925137f5449c11e2667479d68d4812
calumice <99gastgamer81@gmail.com>
2022-09-23 01:27:17 +0900

    fps(仮)クラスの追加
    安藤のコミット

---
+++ Game/Game/FPS.cpp
@@ -0,0 +1,31 @@
+#include "FPS.h"
+#include "DxLib.h"
+
+FPS::FPS()
+	: MSetFps(60)
+	, MOneFrameTickCount(1000 / MSetFps)
+	, mBeforeTickCount(0)
+	, mDeltaTime(0)
+{
+}
+
+void FPS::Update()
+{
+	Wait();
+
+	mDeltaTime = (GetNowCount() - mBeforeTickCount) / 1000.0f;
+	// デルタタイムを制御
+	if (mDeltaTime >= 0.10f)
+	{
+		mDeltaTime = 0.10f;
+	}
+
+	mBeforeTickCount = GetNowCount();
+}
+
+void FPS::Wait()
+{
+	int aiu = GetNowCount();
+	// 現在値が1フレームにかける時間を超えるまで繰り返す
+	while (GetNowCount() < mBeforeTickCount + MOneFrameTickCount);
+}
