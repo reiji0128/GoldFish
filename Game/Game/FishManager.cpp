ad4768c6a68e5b7f9130bcde90ce072bec025221
Nagatomo <reiji128@icloud.com>
2022-09-25 09:44:28 +0900

    FishManagerのコメント追加

--- Game/Game/FishManager.cpp
+++ Game/Game/FishManager.cpp
@@ -14,7 +14,14 @@ FishManager::~FishManager()
 	RemoveAll();
 }
 
-void FishManager::CreatePool(const int redFishSize, const int blueFishSize, const int goldFishSize)
+/// <summary>
+/// 金魚を格納するオブジェクトプールの作成
+/// </summary>
+/// <param name="redFishSize">生成する赤色の金魚の数</param>
+/// <param name="blueFishSize">生成する青色の金魚の数</param>
+/// <param name="goldFishSize">生成する金色の金魚の数</param>
+/// <param name="blackFishSize">生成する黒色の金魚の数</param>
+void FishManager::CreatePool(const int redFishSize, const int blueFishSize, const int goldFishSize, const int blackFishSize)
 {
 	for (int i = 0; redFishSize; i++)
 	{
@@ -33,28 +40,44 @@ void FishManager::CreatePool(const int redFishSize, const int blueFishSize, cons
 	}
 }
 
+/// <summary>
+/// オブジェクトプールに金魚を追加
+/// </summary>
+/// <param name="addFish">追加する金魚</param>
 void FishManager::AddFish(Fish* addFish)
 {
-	mFishList.push_back(addFish);
+	mFishPool.push_back(addFish);
 }
 
+/// <summary>
+/// オブジェクトプールの金魚の削除
+/// </summary>
+/// <param name="removeFish">削除する金魚</param>
 void FishManager::RemoveFish(Fish* addFish)
 {
-	mFishList.pop_back();
+	mFishPool.pop_back();
 }
 
+/// <summary>
+/// オブジェクトプールのすべての要素の削除
+/// </summary>
 void FishManager::RemoveAll()
 {
-	for (int i = 0; i < mFishList.size(); i++)
+	for (int i = 0; i < mFishPool.size(); i++)
 	{
-		delete mFishList.back();
+		delete mFishPool.back();
 	}
 }
 
+/// <summary>
+/// 当たり判定情報の取得
+/// </summary>
+/// <param name="i">取得する要素番号</param>
+/// <returns>当たり判定情報</returns>
 CollisionInfo FishManager::GetCollisionInfo(int i)
 {
-	mCollisionInfo.pos = mFishList[i]->GetPosition();
-	mCollisionInfo.tag = mFishList[i]->GetTag();
+	mCollisionInfo.pos = mFishPool[i]->GetPosition();
+	mCollisionInfo.tag = mFishPool[i]->GetTag();
 
 	return mCollisionInfo;
 }
--- Game/Game/FishManager.h
+++ Game/Game/FishManager.h
@@ -12,22 +12,47 @@ struct CollisionInfo
 	Tag tag;
 };
 
+// 前方宣言
 class Fish;
 class RedFish;
 class BlueFish;
 class GoldFish;
+class BlackFish;
 
 class FishManager final
 {
 public:
 	FishManager();
+
 	~FishManager();
 
+	/// <summary>
+	/// 金魚を格納するオブジェクトプールの作成
+	/// </summary>
+	/// <param name="redFishSize">生成する赤色の金魚の数</param>
+	/// <param name="blueFishSize">生成する青色の金魚の数</param>
+	/// <param name="goldFishSize">生成する金色の金魚の数</param>
+	/// <param name="blackFishSize">生成する黒色の金魚の数</param>
 	void CreatePool(const int redFishSize,
 					const int blueFishSize,
-					const int goldFishSize);
+					const int goldFishSize,
+	                const int blackFishSize);
+
+	/// <summary>
+	/// オブジェクトプールに金魚を追加
+	/// </summary>
+	/// <param name="addFish">追加する金魚</param>
 	void AddFish(Fish* addFish);
+
+	/// <summary>
+	/// オブジェクトプールの金魚の削除
+	/// </summary>
+	/// <param name="removeFish">削除する金魚</param>
 	void RemoveFish(Fish* removeFish);
+
+	/// <summary>
+	/// オブジェクトプールのすべての要素の削除
+	/// </summary>
 	void RemoveAll();
 
 // ゲッター //
@@ -35,7 +60,7 @@ public:
 	/// <summary>
 	/// 当たり判定情報の取得
 	/// </summary>
-	/// <param name="i"></param>
+	/// <param name="i">取得する要素番号</param>
 	/// <returns>当たり判定情報</returns>
 	CollisionInfo GetCollisionInfo(int i);
 
@@ -43,9 +68,11 @@ private:
 	// 当たり判定情報
 	CollisionInfo mCollisionInfo;
 
-	std::vector<Fish*> mFishList;
+	// 金魚のオブジェクトプール
+	std::vector<Fish*> mFishPool;
 
 	RedFish* mRedFish;
 	BlueFish* mBlueFish;
 	GoldFish* mGoldFish;
+	BlackFish* mBlackFish;
 };
