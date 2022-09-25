dd1bc1bfcbc62ad7ca298f374b735262f618c215
Nagatomo <reiji128@icloud.com>
2022-09-26 00:53:41 +0900

    Fishクラスのコメント変更

--- Game/Game/Fish.cpp
+++ Game/Game/Fish.cpp
@@ -12,6 +12,11 @@ Fish::Fish(Tag tag)
     ,mStateNumber(0)
     ,mIsSwim(false)
 {
+	// 座標を最小値から最大値の間でランダムに初期化
+	mPosition.x = (rand() % (mMaxPosX - -mMinPosX + 1)) + -mMinPosX;
+	mPosition.y = (rand() % (mMaxPosY - -mMinPosY + 1)) + -mMinPosY;
+
+	mAngle = 45;
 }
 
 Fish::~Fish()
@@ -34,7 +39,7 @@ void Fish::StopProces()
 	}
 	else
 	{
-		mStateNumber = rand() % 2;
+		mStateNumber = rand() % 10;
 		if (mStateNumber == 0)
 		{
 			mNowState = FishState::Turn;
@@ -58,28 +63,40 @@ void Fish::SwimProces()
 	mNowTime = GetNowCount() / 1000;
 	// 経過時間を計算 
 	mElapsedTime = (mNowTime - mStartTime);
-	//mTime = rand() % 5;
-	if (mElapsedTime < mElapsedSecond)
+
+	// 制限された座標を超えていたらステートをターンに移行
+	if (OverLimitPos())
 	{
-		// 画像の回転角から移動量を計算
-		double delx = -1 * std::cos(90 + Math::Pi / 2);
-		double dely = -1 * std::sin(90 + Math::Pi / 2);
-		mPosition.x += std::lround(delx);
-		mPosition.y += std::lround(dely);
+		mNowState = FishState::Turn;
+		mTargetAngle = mAngle;
 	}
 	else
 	{
-		mStateNumber = rand() % 2;
-		if (mStateNumber == 0)
+		// 経過時間が目標時間を超えるまで移動
+		// 目標時間を超えたら次のステートに移行
+		if (mElapsedTime < mTargetTime)
 		{
-			mNowState = FishState::Stop;
+			// 画像の回転角から移動量を計算
+			double delx = -1 * std::cos(Math::Pi / 4);
+			double dely = -1 * std::sin(Math::Pi / 4);
+			mPosition.x += std::lround(delx);
+			mPosition.y += std::lround(dely);
 		}
 		else
 		{
-			mNowState = FishState::Turn;
+			mStateNumber = rand() % 2;
+			if (mStateNumber == 0)
+			{
+				mNowState = FishState::Stop;
+			}
+			else
+			{
+				mNowState = FishState::Turn;
+			}
+			mIsSwim = false;
 		}
-		mIsSwim = false;
 	}
+
 }
 
 void Fish::TurnProces()
@@ -90,7 +107,7 @@ void Fish::TurnProces()
 	}
 	else
 	{
-		mStateNumber = rand() % 2;
+		mStateNumber = rand() % 10;
 		if (mStateNumber == 0)
 		{
 			mNowState = FishState::Stop;
@@ -101,4 +118,18 @@ void Fish::TurnProces()
 		}
 		mAngle = 0;
 	}
+}
+
+bool Fish::OverLimitPos()
+{
+	// 制限された座標を超えていたら
+	if (mMinPosX > mPosition.x ||
+		mMaxPosX < mPosition.x ||
+		mMinPosY > mPosition.y ||
+		mMaxPosY < mPosition.y)
+	{
+		return true;
+	}
+
+	return false;
 }
--- Game/Game/Fish.h
+++ Game/Game/Fish.h
@@ -17,10 +17,32 @@ public:
 	Fish(Tag tag);
 	~Fish();
 
+	/// <summary>
+	/// 止まっている処理
+	/// </summary>
 	void StopProces();
+
+	/// <summary>
+	/// 泳ぐ処理
+	/// </summary>
 	void SwimProces();
+
+	/// <summary>
+	/// 曲がる処理
+	/// </summary>
 	void TurnProces();
+
 protected:
+
+	/// <summary>
+	/// 制限された座標を超えたか調べる関数
+	/// </summary>
+	/// <returns>
+	/// true  : 制限された座標を超えた
+	/// false : 制限された座標を超えていない
+	/// </returns>
+	bool OverLimitPos();
+
 	// アニメーショングラフハンドル
 	int mGraphHandle[8];
 	// 開始時のタイム
@@ -35,12 +57,18 @@ protected:
 	int mAnimationCount;
 	// ステートの番号
 	int mStateNumber;
-	// 経過時間(秒)
-	const int mElapsedSecond = 2;
+	// 目標時間(秒)
+	const int mTargetTime = 2;
 	// 目標角度
-	const int mTargetAngle = 45;
+	int mTargetAngle;
 	// 泳いでる状態か
 	bool mIsSwim;
+	// 座標の最小値(画面から出ないように0より大きい値)
+	const int mMinPosX = 100;
+	const int mMinPosY = 100;
+	// 座標の最大値(画面から出ないようにフルスクリーンのサイズより小さい値)
+	const int mMaxPosX = 1900;
+	const int mMaxPosY = 1750;
 	// 現在の状態
 	FishState mNowState;
 };
