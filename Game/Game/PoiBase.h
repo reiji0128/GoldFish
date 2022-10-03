6d5d77d78971f8be318f4bf778288df07655995c
lilyLis7216 <silverfish_shion@yahoo.co.jp>
2022-10-03 22:55:56 +0900

    PoiBaseの追加

--- Game/Game/PoiBase.h
+++ Game/Game/PoiBase.h
@@ -1,42 +1,44 @@
 #pragma once
+#include "Tag.h"
 
 class PoiBase
 {
 public:
     /// <summary>
-    /// コンストラクタ.
+    /// コンストラクタ
     /// </summary>
     PoiBase();
 
     /// <summary>
-    /// デストラクタ.
+    /// デストラクタ
     /// </summary>
     virtual ~PoiBase();
 
     /// <summary>
-    /// 更新処理.
+    /// 更新処理
     /// </summary>
     /// <param name="deltaTime">1フレームの経過時間</param>
     void Update(float deltaTime);
 
     /// <summary>
-    /// 描画処理.
+    /// 描画処理
     /// </summary>
     void Draw();
 
+protected:
     /// <summary>
-    /// 移動処理.
+    /// 移動処理
     /// </summary>
     /// <param name="deltaTime">1フレームの経過時間</param>
     void Move(float deltaTime);
 
     /// <summary>
-    /// 位置調整処理.
+    /// 位置調整処理
     /// </summary>
     void AdjustPos();
 
     /// <summary>
-    /// 掬う処理.
+    /// 掬う処理
     /// </summary>
     /// <param name="deltaTime">1フレームの経過時間</param>
     void Scoop(float deltaTime);
@@ -47,62 +49,93 @@ public:
     /// <param name="num">減少させる数値</param>
     void DecHP(int num);
 
+    /// <summary>
+    /// 体力確認処理
+    /// </summary>
     void CheckHP();
 
     /// <summary>
-    /// 修理処理.
+    /// 修理処理
     /// </summary>
     /// <param name="deltaTime">1フレームの経過時間</param>
     void Repair(float deltaTime);
 
     /// <summary>
-    /// ボーナス処理,
+    /// ボーナス処理
     /// </summary>
     /// <param name="deltaTime">1フレームの経過時間</param>
     void Bonus(float deltaTime);
 
-protected:
+    /// <summary>
+    /// スコア計算
+    /// </summary>
+    /// <param name="tag">金魚の種類</param>
+    void CalcScore(Tag tag);
 
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    void Coll();
+
+    // 位置
     float mPosX;
     float mPosY;
 
+    // スケール
     float mScaleX;
     float mScaleY;
 
+    // スケール(変動用)
     float mFloatScale;
 
+    // ハーフスケール（計算用）
     float mHalfScaleX;
     float mHalfScaleY;
 
+    // コントローラーの入力関連
     int mPadNum;
     int mInputX;
     int mInputY;
 
+    // 速度
     float mSpeed;
 
+    // 画像
     int mImage;
 
-    int mBreakImg[5];
+    // 破れ差分
+    int mBreakImg[5] = { -1 };
 
-    int mScoopImg[3];
+    // 掬う差分
+    int mScoopImg[3] = { -1 };
 
+    // 掬っているかどうか
     bool mIsScoop;
 
+    // 1フレーム目かどうか
     bool mIsFirstFrame;
 
+    // 前フレームでの入力していたかどうか
     bool mPrevInput;
 
+    // アニメーション用
     float mAnimCounter;
     int mAnimNum;
 
+    // ボーナス用
     bool mIsBonus;
     int mBonusCount;
-    float mBonusCounter;
+    float mBonusTimer;
 
-    bool mIsDead;
+    // 耐久値
+    int hp;
+
+    // ポイが生きているかどうか
+    bool mIsAlive;
 
     float deadTime;
 
+    // スコア
     int mScore;
 
 };
