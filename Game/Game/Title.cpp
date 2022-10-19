3f843757ffc3b99d1ecab2905cf3803e36e94e3e
406-16 <a@bcs.com>
2022-10-19 19:41:30 +0900

    Titleシーンの変更
    安藤のコミット

--- Game/Game/Title.cpp
+++ Game/Game/Title.cpp
@@ -11,6 +11,8 @@ Title::Title()
 	,mStartBtn(nullptr)
 	// ゲームを閉じるボタンの初期化.
 	,mExitBtn(nullptr)
+	// 現在Joypadがオンマウス状態にある変数の初期化.
+	,mNowBtnState(btnState::Start)
 {
 	// Backgroundクラスのコンストラクタ、引数にタイトルのBackgroundを指定する.
 	mBg = new Background(BgImgName[BgImgFileNum::TitleBg]);
@@ -39,7 +41,23 @@ Title::~Title()
 SceneBase::SceneTag Title::Updata()
 {
 	// 入力処理.
-	return Input();
+	Input();
+
+	// シーンを変えるフラグがtrueの時.
+	if (mChangeSceneFlag)
+	{
+		// タイトルシーンのタグを返す.
+		return SceneTag::TitleTag;
+	}
+	// ゲームを終わらせるフラグがtrueの時.
+	else if (mExitGameFlag)
+	{
+		// ゲームループを終了させるタグを返す.
+		return SceneTag::ExitTag;
+	}
+
+	// それ以外の時は現在のシーンタグを返す.
+	return mNowSceneTag;
 }
 
 /// <summary>
@@ -50,6 +68,7 @@ void Title::Draw()
 	// Backgroundクラスの描画処理.
 	mBg->Draw();
 
+	// buttonクラスの描画処理.
 	mStartBtn->Draw();
 	mExitBtn->Draw();
 }
@@ -57,13 +76,26 @@ void Title::Draw()
 /// <summary>
 /// シーンを変える際の入力処理関数.
 /// </summary>
-SceneBase::SceneTag Title::Input()
+void Title::Input()
 {
 	// 現在のパッドの押したボタンの入力状態を保存する変数.
 	int pad1Input;
 
 	// １つ目のJoyPadの押したボタンの入力状態を取得.
 	pad1Input = GetJoypadInputState(DX_INPUT_PAD1);
+
+	// １つ目のJoyPadが左キーボタンを押していた時.
+	if (pad1Input & PAD_INPUT_LEFT)
+	{
+		// ボタンのステータスをStartに変更する.
+		mNowBtnState = btnState::Start;
+	}
+	// １つ目のJoyPadが右キーボタンを押していた時.
+	else if (pad1Input & PAD_INPUT_RIGHT)
+	{
+		// ボタンのステータスをExitに変更する.
+		mNowBtnState = btnState::Exit;
+	}
 
 	// １つ目のJoyPadがAボタンを押していた時.
 	// デバッグ用としてキーボードで7ボタンを押した時.
@@ -74,16 +106,13 @@ SceneBase::SceneTag Title::Input()
 		if (mNowBtnState == btnState::Start)
 		{
 			// チュートリアルシーンを返す.
-			return SceneTag::TutorialTag;
+			mChangeSceneFlag = true;
 		}
 		// もしボタンのステータスがExitだった時.
 		else if(mNowBtnState == btnState::Exit)
 		{
 			// ゲームを閉じるシーンタグを返す.
-			return SceneTag::ExitTag;
+			mExitGameFlag = true;
 		}
 	}
-
-	// それ以外は自分のシーンを返す.
-	return mNowSceneTag;
 }
