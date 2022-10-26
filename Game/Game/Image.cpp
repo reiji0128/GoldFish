4c2181a2b81dda5b5e76b03822fcb1ffbdd8706b
fca <fca>
2022-10-26 19:44:11 +0900

    Draw関数の変更と変数の追加
    安藤のコミット

--- Game/Game/Image.cpp
+++ Game/Game/Image.cpp
@@ -12,10 +12,34 @@ Image::Image(const char* fileName)
     ,mScale(Vector2::Zero)
     // 画像ハンドル保存変数の初期化.
     ,mImgHandle(-1)
+    // 円形のゲージを表示する変数の初期化.
+    ,mCircePercent(0.0f)
 {
     // 画像ハンドルを読み込む.
     mImgHandle = LoadGraph(fileName);
 
     // 画像ハンドルのサイズの取得.
     GetGraphSizeF(mImgHandle, &mScale.x, &mScale.y);
+}
+
+/// <summary>
+/// ステータス別の描画関数.
+/// </summary>
+/// <param name="state">ViewState型のenum classの変数.</param>
+void Image::Draw(ViewState state)
+{
+    // ステータスが通常の時.
+    if (state == ViewState::Normal)
+    {
+        // 描画処理.
+        DrawGraph((int)mPos.x, (int)mPos.y, mImgHandle, true);
+    }
+    // ステータスが円形のゲージ状の時.
+    else if (state == ViewState::CircleGauge)
+    {
+        // 円形のゲージを保存する変数の代入.
+        mCircePercent += 0.1f;
+        // 描画処理.
+        DrawCircleGaugeF(mPos.x, mPos.y, mCircePercent, mImgHandle, true);
+    }
 }
--- Game/Game/Image.h
+++ Game/Game/Image.h
@@ -2,6 +2,18 @@
 #include "Math.h"
 
 /// <summary>
+/// 画像の描画処理別のステータス.
+/// </summary>
+enum class ViewState : unsigned short
+{
+    None = 0,                 // 画像の描画処理がない時.
+    Normal,                   // 画像の描画処理が普通に表示される時.
+    CircleGauge,              // 画像の描画処理が円形に表示される時.
+
+    MaxDrawState,             // 画像の描画処理のパターン数.
+};
+
+/// <summary>
 /// 画像関連クラス.
 /// </summary>
 class Image
@@ -22,17 +34,19 @@ public :
     /// 更新関数、仮想関数.
     /// </summary>
     /// <param name="deltaTime">float型のデルタタイムの引数.</param>
-    virtual void Updata(float deltaTime) = 0;
+    virtual void Updata(float deltaTime) {};
 
     /// <summary>
-    /// 描画関数、仮想関数.
+    /// ステータス別の描画関数、仮想関数.
     /// </summary>
-    virtual void Draw() = 0;
+    void Draw(ViewState state);
 
 protected:
-    Vector2 mPos;        // 座標保存変数.
-    Vector2 mScale;      // 画像の大きさを保存する変数.
-    int mImgHandle;      // 画像ハンドル保存変数.
+    Vector2 mPos;            // 座標保存変数.
+    Vector2 mScale;          // 画像の大きさを保存する変数.
+    int mImgHandle;          // 画像ハンドル保存変数.
+
+    double mCircePercent;    // 円形のゲージをどの程度まで表示するのかを代入する変数(double型).
 
 public :// -------------------セッター・ゲッター関数--------------------
 
@@ -40,26 +54,17 @@ public :// -------------------セッター・ゲッター関数--------------------
     /// 画像の座標を設定する関数.
     /// </summary>
     /// <param name="pos">Vector2型の変数.</param>
-    void SetPosition(Vector2 pos)
-    {
-        this->mPos = pos;
-    }
+    void SetPosition(Vector2 pos) { this->mPos = pos; }
 
     /// <summary>
     /// 現在の座標を返す.
     /// </summary>
     /// <returns>Vector2型の座標変数を返す.</returns>
-    Vector2 GetPosition()
-    {
-        return this->mPos;
-    }
+    Vector2 GetPosition() { return this->mPos; }
 
     /// <summary>
     /// 現在の画像サイズを返す.
     /// </summary>
     /// <returns>Vector2型の画像の大きさを保存している変数を返す.</returns>
-    Vector2 GetScale()
-    {
-        return this->mScale;
-    }
+    Vector2 GetScale() { return this->mScale; }
 };
